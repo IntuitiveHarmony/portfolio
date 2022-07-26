842b51d503b47a46b8ae4dd53ea0187c4c75c24f
ununique3 <j.horst77@gmail.com>
2022-07-26 14:03:25 -0600

    Nav pictures are up

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,56 @@
 
 //  Function to change class for Nav focus
 //----------------------------------------
-focusNav = () => {
-  $(`#navBtn`).addClass(`focus`)
-}
-blurNav = () => {
-  $(`#navBtn`).removeClass(`focus`)
-}
+// focusNav = () => {
+//   $(`#navBtn`).addClass(`focus`)
+//   console.log(`sweet`)
+// }
+// blurNav = () => {
+//   if ($(`#navBtn`) === `focus`) {
+//
+//   } else {
+//   $(`#navBtn`).removeClass(`focus`)
+//   }
+// }
+
 
 $(() => {
 
 $(`li`).hover(function() {
   $(this).addClass(`focus`)
+  // console.log(`yes`)
 }, function() {
   $(this).removeClass(`focus`)
 })//  Make nav items focus when hovered (change class)
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
