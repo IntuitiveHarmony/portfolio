10e350e6d992fdf2785df6b7a4095d196ac49d5e
ununique3 <j.horst77@gmail.com>
2022-07-27 15:05:01 -0600

    getting organized

--- portfolio.js
+++ portfolio.js
@@ -1,28 +1,104 @@
 
-//  Function to change class for Nav focus
-//----------------------------------------
-// focusNav = () => {
-//   $(`#navBtn`).addClass(`focus`)
-//   console.log(`sweet`)
-// }
-// blurNav = () => {
-//   if ($(`#navBtn`) === `focus`) {
-//
-//   } else {
-//   $(`#navBtn`).removeClass(`focus`)
-//   }
-// }
+const clearHome = () => {
+  $(`.main`).empty()
+}
+
+
+//-------------
+//  Welcome page
+//-------------
+
+const loadHome = () => {
+  $(`.main`).empty()
+  let $container = $(`<div>`)
+    .addClass(`container`)
+    .appendTo(`.main`)
+  let $home = $(`<h3>`)
+    .text(`Welcome`)
+    .appendTo($container)
+  let $message = $(`<p>`)
+    .text(`You have stumbled upon my info page`)
+    .appendTo($container)
+}
+
+
+//-----------
+//  Bio Page
+//-----------
+
+const loadBio = () => {
+  clearHome()
+  let $container = $(`<div>`)
+    .addClass(`container`)
+    .appendTo(`.main`)
+  let $bioHeader = $(`<h3>`)
+    .text(`Constant Exploration`)
+    .appendTo($container)
+  let $bio = $(`<p>`)
+    .text(`This is where My bio will go`)
+    .appendTo($container)
+}
+
+
+//--------------
+//  Resume page
+//--------------
+
+const loadResume = () => {
+
+}
+
+
+//--------------
+//  Projects page
+//--------------
+
+const loadProjects = () => {
+
+}
+
+
+//--------------
+//  Resume page
+//--------------
+
+const loadResume = () => {
+
+}
+
+
+//--------------------------------------------------
+//  Add and remove the text of nav items with hover
+//-------------------------------------------------
+const hoverTextAdd = () => {
+  $(`.focus`).siblings().show()
+}
+const hoverTextRemove = () => {
+  $(`h2`).hide()
+  console.log(`txtrm`)
+}
+
 
 
 $(() => {
+  loadHome() //Start with the welcome page
+  $(`.homeLink`).on(`click`, loadHome)  //  Home Button
+  $(`#nav1`).on(`click`, loadBio)       //  Bio Button
+  $(`#nav2`).on(`click`, loadResume)    //  Resume Button
+  $(`#nav3`).on(`click`, loadProjects)  //  Projects Button
+  $(`#nav4`).on(`click`, loadLinks)     //  Bio Button
 
-$(`li`).hover(function() {
-  $(this).addClass(`focus`)
-  // console.log(`yes`)
-}, function() {
-  $(this).removeClass(`focus`)
-})//  Make nav items focus when hovered (change class)
 
+    //---------------------------------------------------
+    //  nav items focus when hovered (change class)
+    //---------------------------------------------------
+  $(`.navPic`).hover(function() {
+    $(this).addClass(`focus`)
+    hoverTextAdd()
+      }, function() {
+        $(this).removeClass(`focus`).removeAttr(`id`, `current`)
+        hoverTextRemove()
+    })
 
 })
 
